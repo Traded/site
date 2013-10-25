e9a3283372d5b150f8076dbc8e555115e2dd0333
James Purdy <james@eviljames.ca>
2013-10-25 00:12:18 -0600

    Bringing in the bootstrap, making the rough base template.

--- traded_io/traded_io/urls.py
+++ traded_io/traded_io/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'social/', include('social.apps.django_app.urls', namespace='social'))
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'api/', include('api.urls', namespace='api')),
+    url(r'social/', include('social.apps.django_app.urls', namespace='social')),
 )
